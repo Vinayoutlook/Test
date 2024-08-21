f36dccb7f3a7ad47abb5bce857595e358391d601
vinay singh <singhvnkmr@gmail.com>
2024-08-21 19:37:18 +0530

    Second commit

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main{
     public static void main(String[] args){
         System.out.println("Welcome to jenkins");
+        System.out.println("In jenkins second line");
+        System.out.println("In jenkins third line");
+        System.out.println("In jenkins fourth line");
     }
 }
